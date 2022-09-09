c0205b72bf2662b5855bf0f205a824e2c58e6daf
mossherder <markmendez777@gmail.com>
2022-09-09 12:37:12 -0600

    * add minimal doc viewer with resume from azure st
    * generate quote on mounted
    
    Update README.md
    
    * update favicon and page title
    
    * typo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="quote-section">
+      <h4>{{ quote }}</h4>
+    </div>
+    <SimpleDocViewer id="resume"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleDocViewer from './components/SimpleDocViewer.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SimpleDocViewer
+  },
+  data() {
+    return {
+      quote: ""
+    }
+  },
+  mounted: function () {
+    this.randomQuote();
+  },
+  methods: {
+    randomQuote: function () {
+      console.log('getting new quote');
+      axios.get('https://quotable.io/random').then((r) => {
+        this.quote = r.data.content
+      });
+    }
   }
 }
 </script>
 
 <style>
+body {
+  overflow: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#quote-section {
+  display: inline-block;
+}
+#resume {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
---
+++ src/components/SimpleDocViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe id="frame" src="https://docs.google.com/gview?url=https://simpleresumeraccount.blob.core.windows.net/resume-blob/Mendez_Resume.docx&embedded=true"></iframe>
+</template>
+<script>
+
+export default {
+  name: 'SimpleDocViewer',
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#frame {
+  width: 100%;
+  height: 100%;
+}
+</style>
